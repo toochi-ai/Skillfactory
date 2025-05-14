32e9d02b39a41f1beb93271990c576f073be3d4c
toochi-ai <mikeilov.ea@gmail.com>
2025-05-14 23:44:03 +0300

    Извлечение постов по сходству

--- project_django/mysite/blog/views.py
+++ project_django/mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
@@ -7,6 +8,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -44,11 +46,17 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
